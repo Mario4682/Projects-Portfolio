3bbce01e8096ba65d69e0396bf635075caf4c030
Mario4682 <58978669+Mario4682@users.noreply.github.com>
2023-12-13 08:57:24 -0800

    Add files via upload

---
+++ scirpt.js
@@ -0,0 +1,6 @@
+// script.js
+
+document.getElementById('contact-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    alert('Message sent!'); // You can replace this with your actual contact form submission logic
+});
